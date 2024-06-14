d9310c6258f9cec7f0a3c4edc16bf271219e92ad
Gabriel Ferreira <gf-castro2012@hotmail.com>
2024-06-14 13:47:31 +0000

    Alterações para deploy

--- resources/views/avaliacao.blade.php
+++ resources/views/avaliacao.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/app.js','resorces/sass/app.scss'])
     <link rel="stylesheet" href="/CSS/avaliacao.css">
     <title>Avaliação</title>
 </head>
